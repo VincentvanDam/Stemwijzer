f6f1696f0b79a0e1a82c92bf5c6b53e8e26f20b4
VincentvanDam <vincent.vandam@live.nl>
2018-02-07 10:47:22 +0100

    Andere manier gebruikt

--- js/stemwijzer.js
+++ js/stemwijzer.js
@@ -1,22 +1,21 @@
-document.getElementById('subject').innerHTML = 'StemWijzer Tweede Kamer 2017';
-document.getElementById('statement').innerHTML = 'Test uw politieke voorkeur aan de hand van 30 stellingen';
+var nr = 0;
 
+var subject = document.getElementById('subject');
+var statement = document.getElementById('statement');
+var next = document.getElementById('skip');
+var back = document.getElementById('back');
 
+subject.innerHTML = subjects[nr].title;
+statement.innerHTML = subjects[nr].statement;
 
-function vraag1() {
-	var nr = 0;
-	var text = document.getElementById('subject');
-	text.innerHTML = subjects[nr].title;
-	
-	var text = document.getElementById('statement');
-    text.innerHTML = subjects[nr].statement;
+var next = function(event){
+    nr++;
+    subject.innerHTML = subjects[nr].title;
+    statement.innerHTML = subjects[nr].statement;
+}
 
-    document.getElementById('option1').innerHTML = 'Eens';
-    document.getElementById('option2').innerHTML ='Geen van beide';
-    document.getElementById('option3').innerHTML ='Mee oneens'
-    document.getElementById('option4').innerHTML ='Sla deze vraag over'
-
-    document.getElementById('option2').style.display ="block";
-    document.getElementById('option3').style.display ="block";
-    document.getElementById('option4').style.display ="block";
+var prev = function(event){
+    nr--;
+    subject.innerHTML = subjects[nr].title;
+    statement.innerHTML = subjects[nr].statement;
 }
