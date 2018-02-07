aafab4b2cac615d8f8ebb705ea098ca7dbf6fadd
VincentvanDam <vincent.vandam@live.nl>
2018-02-07 16:17:07 +0100

    no message

--- js/stemwijzer.js
+++ js/stemwijzer.js
@@ -1,6 +1,26 @@
 var nr = 0;
 
-scores =[{party: 'pvv', total: 0},{party: 'vvd', total: 0}];
+scores =[{party: 'pvv', total: 0},
+         {party: 'vvd', total: 0},
+         {party: 'cda', total: 0},
+         {party: 'd66', total: 0},
+         {party: 'groenlinks', total: 0},
+         {party: 'sp', total: 0},
+         {party: 'pvda', total: 0},
+         {party: 'christenunie', total: 0},
+         {party: 'partij voor de dieren', total: 0},
+         {party: 'sgp', total: 0},
+         {party: 'denk', total: 0},
+         {party: 'forum voor democratie', total: 0},
+         {party: 'lokaal in de kamer', total: 0},
+         {party: 'ondernemerspartij', total: 0},
+         {party: 'vnl', total: 0},
+         {party: 'nieuwe wegen', total: 0},
+         {party: 'de burger beweging', total: 0},
+         {party: 'piratenpartij', total: 0},
+         {party: 'artikel 1', total: 0},
+         {party: 'liberatische partij', total: 0}]
+         
 
 var subject = document.getElementById('subject');
 var statement = document.getElementById('statement');
@@ -27,10 +47,14 @@ var prev = function(event){
 }
 
 var agree = function(event){
+    pro = true;
+    if (parties.position === true){
+        scores.total +1;
+    }
     nr++;
     subject.innerHTML = subjects[nr].title;
     statement.innerHTML = subjects[nr].statement;
-    console.dir(scores);
+    console.dir(subjects.parties.position);
 }
 
 var disagree = function(event){
