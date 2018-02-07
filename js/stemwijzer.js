bad7390f5534cd7a34cb5d5b3ef65973c0967409
VincentvanDam <vincent.vandam@live.nl>
2018-02-07 14:13:55 +0100

    no message

--- js/stemwijzer.js
+++ js/stemwijzer.js
@@ -1,9 +1,15 @@
 var nr = 0;
 
+scores =[{party: 'pvv', total: 0},{party: 'vvd', total: 0}];
+
 var subject = document.getElementById('subject');
 var statement = document.getElementById('statement');
 var next = document.getElementById('skip');
 var back = document.getElementById('back');
+var agree = document.getElementById('agree')
+var disagree = document.getElementById('disagree')
+var pro;
+var contra;
 
 subject.innerHTML = subjects[nr].title;
 statement.innerHTML = subjects[nr].statement;
@@ -18,4 +24,19 @@ var prev = function(event){
     nr--;
     subject.innerHTML = subjects[nr].title;
     statement.innerHTML = subjects[nr].statement;
+}
+
+var agree = function(event){
+    nr++;
+    subject.innerHTML = subjects[nr].title;
+    statement.innerHTML = subjects[nr].statement;
+    console.dir(scores);
+}
+
+var disagree = function(event){
+    contra = true;
+    nr++
+    subject.innerHTML = subjects[nr].title;
+    statement.innerHTML = subjects[nr].statement;
+    console.dir(scores);
 }
